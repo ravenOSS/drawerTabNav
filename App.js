c8dd720af97c71ceac32622b17bdc04c45f16da5
David Richards <35054853+ravenOSS@users.noreply.github.com>
2021-06-07 15:14:43 -0600

    Feat: Remove navigation logic from App.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import { createDrawerNavigator } from '@react-navigation/drawer'
 import { NavigationContainer } from '@react-navigation/native'
+import DrawerNavigator from './navigation/DrawerNavigator'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
 import * as React from 'react'
 import {
 	DefaultTheme,
@@ -7,9 +9,6 @@ import {
 	useTheme,
 	Provider as PaperProvider,
 } from 'react-native-paper'
-import TabNav from './navigation/TabNav'
-import About from './screens/About'
-import Settings from './screens/Settings'
 
 const theme = {
 	...DefaultTheme,
@@ -22,66 +21,14 @@ const theme = {
 	},
 }
 
-const screenOptionStyle = {
-	headerStyle: {
-		backgroundColor: '#c0ca33',
-	},
-	headerTintColor: '#fff',
-	headerTitleStyle: {
-		fontSize: 25,
-		fontWeight: '700',
-	},
-	headerMode: 'screen',
-	headerTitleAlign: 'center',
-}
-
-const drawerStyle = {
-	paddingTop: 40,
-	backgroundColor: '#000',
-	width: 240,
-}
-
-const ContentOptions = {
-	activeTintColor: '#c0ca33',
-	activeBackgroundColor: '#000',
-	inactiveTintColor: '#fb8c00',
-	inactiveBackgroundColor: '#000',
-	labelStyle: {
-		fontSize: 20,
-		fontWeight: 'bold',
-	},
-	itemStyle: { marginVertical: 20 },
-}
-
-const Drawer = createDrawerNavigator()
-
 export default function App() {
-	const { colors } = useTheme()
 	return (
-		<PaperProvider theme={theme}>
-			<NavigationContainer>
-				<Drawer.Navigator
-					screenOptions={screenOptionStyle}
-					drawerStyle={drawerStyle}
-					drawerContentOptions={ContentOptions}
-				>
-					<Drawer.Screen
-						name='Home'
-						component={TabNav}
-						options={{ title: 'Home' }}
-					/>
-					<Drawer.Screen
-						name='Settings'
-						component={Settings}
-						options={{ title: 'Settings' }}
-					/>
-					<Drawer.Screen
-						name='About'
-						component={About}
-						options={{ title: 'About' }}
-					/>
-				</Drawer.Navigator>
-			</NavigationContainer>
-		</PaperProvider>
+		<SafeAreaProvider>
+			<PaperProvider theme={theme}>
+				<NavigationContainer>
+					<DrawerNavigator />
+				</NavigationContainer>
+			</PaperProvider>
+		</SafeAreaProvider>
 	)
 }
